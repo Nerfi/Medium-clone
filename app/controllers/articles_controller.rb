bde32e652d22c1f4e853bde61fa954212ec06f8c
Juan Paredes <juanchuu.jfpc@gmail.com>
2019-09-03 18:57:20 +0200

    articles controller done, new. only new and index routes implemented, need to add validations to the article model and improve a bit the UI

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,66 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show,:edit,:update, :destroy]
+  skip_before_action :authenticate_user!, only: [:index, :show]
+
+  def index
+    @articles = Article.all
+
+  end
+
+
+  def show
+    #@article = Article.find(params[:id])
+
+  end
+
+  def new
+    @article = Article.new
+
+  end
+
+  def create
+
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to articles_path(@articles)
+    else
+      render :new
+    end
+    @article.errors
+
+  end
+
+  def edit
+    #@article = Article.find(params[:id])
+
+  end
+
+  def update
+   # @article = Article.find(params[:id])
+    @article.update(article_params)
+
+    redirect_to article_path(@article)
+
+  end
+
+  def destroy
+    #@article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
+
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title,:description)
+
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
+
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_03_150151) do
+ActiveRecord::Schema.define(version: 2019_09_03_164131) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,10 +18,8 @@ ActiveRecord::Schema.define(version: 2019_09_03_150151) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
-    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -36,5 +34,4 @@ ActiveRecord::Schema.define(version: 2019_09_03_150151) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "articles", "users"
 end
