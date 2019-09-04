d42a33a3ebe774c2562b05059ddab76de50cf45c
Juan Paredes <juanchuu.jfpc@gmail.com>
2019-09-04 13:16:25 +0200

    article pundit set up, need to configured the article controller

--- app/policies/article_policy.rb
+++ app/policies/article_policy.rb
@@ -4,4 +4,48 @@ class ArticlePolicy < ApplicationPolicy
       scope.all
     end
   end
+
+  #quien puede crear un article ?
+  def new?
+    return true
+    #everyone
+
+  end
+
+  def create?
+    return new?
+
+  end
+
+  def show?
+    #everyone can see a restaurant
+    return true
+
+  end
+
+  def update?
+    user_is_owner?
+    #record.user == user
+    #solo pueden hacer 'update'
+    #aquellos que hallan creado el article in this case
+
+  end
+
+  def destroy
+    user_is_owner?
+
+  end
+
+  private
+
+  def user_is_owner?
+    record.user ==  user
+
+  end
+
+
+
+
+
+
 end
