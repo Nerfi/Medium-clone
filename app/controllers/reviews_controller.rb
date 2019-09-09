eca7fd64c5a06f8fd7d7a69a9d394255a08f9002
Juan Paredes <juanchuu.jfpc@gmail.com>
2019-09-09 17:57:30 +0200

    added ajax to reviewa and articles, need to check if its working tho

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -10,9 +10,21 @@ class ReviewsController < ApplicationController
     @review = Review.new(review_params)
     @article = Article.find(params[:article_id])
     @review.article = @article
-    @review.save
 
-    redirect_to article_path(@article)
+      if @review.save
+        respond_to do |format|
+          format.html { redirect_to article_path(@article) }
+          format.js  # <-- will render `app/views/reviews/create.js.erb`
+        end
+
+
+    else
+      respond_to do |format|
+      format.html {render 'articles/show'}
+      format.js
+      end
+
+    end
 
   end
 
