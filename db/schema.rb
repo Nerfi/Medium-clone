b3c4029ed1b1894f40a54699caf09dcff4afcdaf
Juan Paredes <juanchuu.jfpc@gmail.com>
2019-09-09 15:54:46 +0200

    addede reviews routes create and destroy and also review model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_09_104602) do
+ActiveRecord::Schema.define(version: 2019_09_09_134912) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,14 @@ ActiveRecord::Schema.define(version: 2019_09_09_104602) do
     t.datetime "updated_at", null: false
     t.string "photo"
     t.index ["user_id"], name: "index_articles_on_user_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text "content"
+    t.bigint "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_reviews_on_article_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -38,4 +46,5 @@ ActiveRecord::Schema.define(version: 2019_09_09_104602) do
   end
 
   add_foreign_key "articles", "users"
+  add_foreign_key "reviews", "articles"
 end
