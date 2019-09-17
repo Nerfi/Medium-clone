1a2eba39b23c52b26aeb37eef26abf836e3cd3c1
Juan Paredes <juanchuu.jfpc@gmail.com>
2019-09-17 18:33:20 +0200

    unauthorize erro in user index, no clue whats wrong

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,19 @@
 class UsersController < ApplicationController
    before_action :set_user, only: [:follow, :unfollow]
 
+
    def index
-    @users = User.where.not(id: current_user.id)
-    authorize @users
+     @users = User.where.not(id: current_user.id)
+
+     authorize @users
+
+
   end
 
   def follow
     if current_user.follow(@user.id)
+
+
       respond_to do |format|
         format.html { redirect_to root_path }
         format.js
--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -3,5 +3,25 @@ class UserPolicy < ApplicationPolicy
     def resolve
       scope.all
     end
+
+    def index?
+      return true
+
+    end
+
+
+
+    def follow?
+       true
+
+    end
+
+    def unfollow?
+        return follow?
+
+    end
+
+
+
   end
 end
